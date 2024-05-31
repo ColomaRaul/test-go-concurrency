507454cba7426e718eee0df3f160f569d48e9a27
Raúl Coloma <rau17cb@gmail.com>
2024-05-31 18:56:09 +0200

    initial commit

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/prices"
 )
 
-func greet(phrase string) {
-	fmt.Println("Hello!", phrase)
-}
-
-func slowGreet(phrase string) {
-	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
-	fmt.Println("Hello!", phrase)
-}
-
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
+	}
 }
